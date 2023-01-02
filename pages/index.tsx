a18bf79ba7f213745a843378c3c2a5b9fc6ca29b
bbfrancis23 <bbfrancis23@gmail.com>
2023-01-02 13:15:25 -0700

    home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,152 @@
 import dbConnect from "../lib/dbConnect";
-
+import Image from 'next/image'
 import Tags from "../models/Tags";
 import TagTypes from '../models/TagTypes'
 import useSWR from 'swr'
 import axios from "axios";
+import { Box, Grid, Card, CardHeader, CardActions, Button, CardMedia } from "@mui/material";
 
 export default function Home() {
   return (
-    <>
-      <h1>The Home page</h1>
-     </>
+    
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={8} lg={4} >
+          <Card >
+            <CardHeader title='JavaScript'>          </CardHeader>
+            <CardMedia>
+              <div style={{ position: 'relative', width: '100%', height: '300px' }}>
+                <Image alt="javascript" src={'/images/card-dogs/javascript.jpg'} layout="fill" objectFit="cover" />
+              </div>
+            </CardMedia>
+            <CardActions>
+              <Button>Standards</Button>
+              <Button>Best Practices</Button>
+              <Button>Examples</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={8} lg={4} >
+          <Card >
+            <CardHeader title='HTML'></CardHeader>
+            <CardMedia>
+              <div style={{ position: 'relative', width: '100%', height: '300px' }}>
+                <Image alt="javascript" src={'/images/card-dogs/html.jpg'} layout="fill" objectFit="cover" />
+              </div>
+            </CardMedia>
+            <CardActions>
+              <Button>Standards</Button>
+              <Button>Best Practices</Button>
+              <Button>Examples</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={8} lg={4} >
+          <Card >
+            <CardHeader title='CSS'></CardHeader>
+            <CardMedia>
+              <div style={{ position: 'relative', width: '100%', height: '300px' }}>
+                <Image alt="javascript" src={'/images/card-dogs/css.jpg'} layout="fill" objectFit="cover" />
+              </div>
+            </CardMedia>
+            <CardActions>
+              <Button>Standards</Button>
+              <Button>Best Practices</Button>
+              <Button>Examples</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={8} lg={4} >
+          <Card >
+            <CardHeader title='React'></CardHeader>
+            <CardMedia>
+              <div style={{ position: 'relative', width: '100%', height: '300px' }}>
+                <Image alt="javascript" src={'/images/card-dogs/react.jpg'} layout="fill" objectFit="cover" />
+              </div>
+            </CardMedia>
+            <CardActions>
+              <Button>Standards</Button>
+              <Button>Best Practices</Button>
+              <Button>Examples</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={8} lg={4} >
+          <Card >
+            <CardHeader title='NextJS'></CardHeader>
+            <CardMedia>
+              <div style={{ position: 'relative', width: '100%', height: '300px' }}>
+                <Image alt="javascript" src={'/images/card-dogs/nextjs.jpg'} layout="fill" objectFit="cover" />
+              </div>
+            </CardMedia>
+            <CardActions>
+              <Button>Standards</Button>
+              <Button>Best Practices</Button>
+              <Button>Examples</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={8} lg={4} >
+          <Card >
+            <CardHeader title='Material User Interface'></CardHeader>
+            <CardMedia>
+              <div style={{ position: 'relative', width: '100%', height: '300px' }}>
+                <Image alt="javascript" src={'/images/card-dogs/mui.jpg'} layout="fill" objectFit="cover" />
+              </div>
+            </CardMedia>
+            <CardActions>
+              <Button>Standards</Button>
+              <Button>Best Practices</Button>
+              <Button>Examples</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={8} lg={4} >
+          <Card >
+            <CardHeader title='NodeJS'></CardHeader>
+            <CardMedia>
+              <div style={{ position: 'relative', width: '100%', height: '300px' }}>
+                <Image alt="javascript" src={'/images/card-dogs/nodejs.jpg'} layout="fill" objectFit="cover" />
+              </div>
+            </CardMedia>
+            <CardActions>
+              <Button>Standards</Button>
+              <Button>Best Practices</Button>
+              <Button>Examples</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={8} lg={4} >
+          <Card >
+            <CardHeader title='Express'></CardHeader>
+            <CardMedia>
+              <div style={{ position: 'relative', width: '100%', height: '300px' }}>
+                <Image alt="javascript" src={'/images/card-dogs/express.jpg'} layout="fill" objectFit="cover" />
+              </div>
+            </CardMedia>
+            <CardActions>
+              <Button>Standards</Button>
+              <Button>Best Practices</Button>
+              <Button>Examples</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={8} lg={4} >
+          <Card >
+            <CardHeader title='MongoDB'></CardHeader>
+            <CardMedia>
+              <div style={{ position: 'relative', width: '100%', height: '300px' }}>
+                <Image alt="javascript" src={'/images/card-dogs/mongodb.jpg'} layout="fill" objectFit="cover" />
+              </div>
+            </CardMedia>
+            <CardActions>
+              <Button>Standards</Button>
+              <Button>Best Practices</Button>
+              <Button>Examples</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
+    
   )
 }
 export async function getStaticProps() {
--- pages/items/index.js
+++ pages/items/index.js
@@ -1,4 +1,21 @@
-function Items() {
+import axios from 'axios';
+
+export default function Items() {
   return <div>All Items</div>;
 }
-export default Items;
+export async function getStaticProps() {
+  let items;
+  try {
+    items = await axios.get('http://localhost:5000/api/items');
+
+    console.log(items.data);
+  } catch (err) {
+    console.log(err);
+  }
+
+  return {
+    props: {
+      tags: {},
+    },
+  };
+}
