87447b0ac7f538906dac704610c5f1bed96941d2
bbfrancis23 <bbfrancis23@gmail.com>
2023-03-06 23:00:39 -0700

    again

--- utils/db.js
+++ utils/db.js
@@ -7,7 +7,7 @@ async function connect() {
     console.log('already connected');
     return;
   }
-  // here is a comment
+  // here is a comment more comments
   if (mongoose.connections.length > 0) {
     connection.isConnected = mongoose.connections[0].readyState;
 
