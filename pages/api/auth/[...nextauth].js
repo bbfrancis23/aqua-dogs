bca40628cc7f8d4d0821ed7ef29978465514d59e
bbfrancis23 <bbfrancis23@gmail.com>
2023-03-30 15:11:33 -0700

    fix

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -19,23 +19,20 @@ export default NextAuth({
     async session({ session, token }) {
       if (token?._id) session.member._id = token._id;
 
-
       await db.connect();
       console.log('trying to get seesion');
 
       console.log('session', session);
 
-      try {
-        const member = await Member.findOne({
-          email: session.user.email,
-        }).populate({ path: 'roles', model: Role });
+      const member = await Member.findOne({
+        email: session.user.email,
+      }).populate({ path: 'roles', model: Role });
 
-        const roles = member.roles.map((r) => r.title);
+      const roles = member.roles.map((r) => r.title);
 
       session.user.id = token.sub;
 
       session.user.roles = roles;
-
 
       console.log('session:', session);
       await db.disconnect();
