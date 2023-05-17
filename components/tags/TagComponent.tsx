26ffd3a90c9169faf2982c0b571b455b05998fc8
bbfrancis23 <bbfrancis23@gmail.com>
2023-05-17 14:38:19 -0700

    ready to merge

--- components/tags/TagComponent.tsx
+++ components/tags/TagComponent.tsx
@@ -1,19 +1,21 @@
-import { useState } from "react";
-import axios from "axios";
-import { useSnackbar } from "notistack";
+import { useState } from "react"
+import axios from "axios"
+import { useSnackbar } from "notistack"
 
-import { Box, IconButton, Typography } from "@mui/material";
-import AddItemIcon from '@mui/icons-material/PostAdd';
+import { Box, IconButton, Typography } from "@mui/material"
+import AddItemIcon from '@mui/icons-material/PostAdd'
 
-import { Tag } from "../../interfaces/TagInterface";
-import { Org } from "../../interfaces/OrgInterface";
-import { TagItems, getTagItems } from "../../interfaces/TagItems";
-import ItemFormDialog from "../items/ItemFormDialog";
+import { Tag } from "../../interfaces/TagInterface"
+import { Org } from "../../interfaces/OrgInterface"
+import { TagItems, getTagItems } from "../../interfaces/TagItems"
 
-import FormModes from "../../enums/FormModes";
-import TagBoard from "./TagBoard";
-import Permission from "../../ui/Permission";
-import PermissionCodes from "../../enums/PermissionCodes";
+import FormModes from "../../enums/FormModes"
+import PermissionCodes from "../../enums/PermissionCodes"
+
+import ItemFormDialog from "../items/ItemFormDialog"
+import TagBoard from "./TagBoard"
+
+import Permission from "../../ui/Permission"
 
 export interface TagComponentProps {
   tag: Tag ;
@@ -44,6 +46,19 @@ const TagsComponent = (props: TagComponentProps) => {
     setSelectedTagIds([tag.id])
   }
 
+
+  const handleUpdateBoardFromDataBase = () => {
+    axios.get(`/api/items/tags/${tag.id}`).then((res) => {
+      if(res.status === axios.HttpStatusCode.Ok){
+        setTagItems(getTagItems(tag, res.data.items))
+      }else{
+        enqueueSnackbar(`Error getting Tagged Items1: ${res.data.message}`, {variant: "error"})
+      }
+    }).catch((error) => {
+      enqueueSnackbar(`Error getting Tagged Items2: ${error}`, {variant: "error"})
+    })
+  }
+
   const handleOpenDialog = (tagId?: string ) => {
     if(tagId){
       setSelectedTagIds([tag.id, tagId])
@@ -67,7 +82,8 @@ const TagsComponent = (props: TagComponentProps) => {
         </Box>
       </Box>
       <TagBoard tagItems={tagItems} tag={tag}
-        setItemFormDialogOpen={(id) => handleOpenDialog(id)} />
+        setItemFormDialogOpen={(id) => handleOpenDialog(id)}
+        updateBoardFromDataBase={() => handleUpdateBoardFromDataBase()} />
       <ItemFormDialog mode={FormModes.ADD} dialogIsOpen={addItemDialogIsOpen}
         closeDialog={handleCloseDialog} tagIds={selectedTagIds} org={org ? org : undefined} />
     </Box>
