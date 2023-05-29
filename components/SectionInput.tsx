64cab9ecac8e93f8ae91ec372ab7d61ae864b0f8
bbfrancis23 <bbfrancis23@gmail.com>
2023-05-29 16:34:47 -0700

    ready to merge

--- components/SectionInput.tsx
+++ components/SectionInput.tsx
@@ -1,13 +1,11 @@
 import {useState, useMemo} from "react"
-import {Button, IconButton, FormControl, InputLabel, OutlinedInput, Stack, InputAdornment,
-  ButtonGroup} from "@mui/material"
 
-
-import DeleteIcon from "@mui/icons-material/Delete"
-import axios from "axios"
+import {Button, IconButton, FormControl, InputLabel, OutlinedInput, Stack,
+  InputAdornment, ButtonGroup, Box} from "@mui/material"
 
 import {useSnackbar} from "notistack"
-
+import axios from "axios"
+import DeleteIcon from "@mui/icons-material/Delete"
 
 import dynamic from "next/dynamic"
 import "@uiw/react-textarea-code-editor/dist.css"
@@ -16,75 +14,41 @@ const CodeEditor = dynamic(
   {ssr: false}
 )
 
-/*eslint-disable */
 
 export default function SelectionInput(props: any){
 
   const {enqueueSnackbar} = useSnackbar()
-
-  // TODO Create section type interface
-
   const {item, setItem, index, section} = props
-
   const [sectionType, setSectionType] = useState("text")
-
   const [code, setCode] = useState("")
 
   useMemo(
     () => {
-
-      if(section.sectiontype === "63b2503c49220f42d9fc17d9"){
-        setSectionType("text")
-      }else{
+      if(section.sectiontype === "63b2503c49220f42d9fc17d9") setSectionType("text")
+      else{
         setSectionType("code")
         setCode(section.content)
       }
-
-
     }, [section]
   )
 
   const handleDeleteSection = (id: string) => {
     try {
       axios.delete(`/api/sections/${id}`)
-        .then((res) => {
-          setItem(res.data.item)
-        })
-        .catch((error) => {
-          console.log(error)
-        })
-    } catch (e) {
-      console.log(e)
-    }
-
+        .then((res) => setItem(res.data.item))
+        .catch((error) => console.log(error))
+    } catch (e) { console.log(e) }
   }
 
-  const handleSectionBlur = async (
-    event: any, section: any
-  ) => {
-
-
+  const handleSectionBlur = ( event: any, section: any ) => {
     try {
       axios.patch(
-        `/api/sections/${section.id}`, {
-          content: event.target.value,
-          sectiontype: sectionType ===
-            "text" ? "63b2503c49220f42d9fc17d9" : "63b88d18379a4f30bab59bad",
-        }
-      )
-        .then((res) => {
-          setItem(res.data.item)
-        })
-        .catch((e:any) => {
-          enqueueSnackbar(
-            e, {variant: "error"}
-          )
-        })
-    } catch (e:any) {
-      enqueueSnackbar(
-        e, {variant: "error"}
-      )
-    }
+        `/api/sections/${section.id}`,
+        { content: event.target.value, sectiontype: sectionType === "text"
+          ? "63b2503c49220f42d9fc17d9" : "63b88d18379a4f30bab59bad", } )
+        .then((res) => setItem(res.data.item) )
+        .catch((e:any) => { enqueueSnackbar( e, {variant: "error"} ) })
+    } catch (e:any) { enqueueSnackbar( e, {variant: "error"} ) }
   }
 
   return (
@@ -132,7 +96,7 @@ export default function SelectionInput(props: any){
       }
       {
         sectionType === "code" && (
-          <>
+          <Box sx={{ display: 'flex', }}>
             <CodeEditor
               value={code}
               language="jsx"
@@ -143,18 +107,19 @@ export default function SelectionInput(props: any){
               )}
               padding={15}
               style={{
+                width: '100%',
                 fontSize: 12,
                 backgroundColor: "#f5f5f5",
                 fontFamily:
                   "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace"
               }}
             />
-          </>
+            <IconButton edge="end" onClick={() => handleDeleteSection(section.id)}>
+              <DeleteIcon />
+            </IconButton>
+          </ Box>
         )
       }
     </Stack>
-
-
   )
-
 }
