99284824300caa39ae0c33c644c3bee6e4c96691
bbfrancis23 <bbfrancis23@gmail.com>
2023-06-22 18:26:18 -0700

    more fixing

--- interfaces/TagItems.ts
+++ interfaces/TagItems.ts
@@ -10,28 +10,27 @@ export const getTagItems = (tag: Tag, items: Item[]) => {
   let tagItems: TagItems[] = [];
 
   items.forEach((i: Item) => {
-    if (i.tags) {
-      i.tags.forEach((t: Tag) => {
-        if (t.id !== tag.id) {
-          let found = false;
-          let index: number | null = null;
-          tagItems.forEach((ti: TagItems, tiIndex: number) => {
-            if (ti.tag.id === t.id) {
-              found = true;
-              index = tiIndex;
-            }
-          });
-
-          if (!found) {
-            tagItems.push({ tag: t, items: [i] });
-          } else {
-            if (index !== null) {
-              tagItems[index].items.push(i);
-            }
-          }
-        }
-      });
-    }
+    // if (i.tags) {
+    //   i.tags.forEach((t: Tag) => {
+    //     if (t.id !== tag.id) {
+    //       let found = false;
+    //       let index: number | null = null;
+    //       tagItems.forEach((ti: TagItems, tiIndex: number) => {
+    //         if (ti.tag.id === t.id) {
+    //           found = true;
+    //           index = tiIndex;
+    //         }
+    //       });
+    //       if (!found) {
+    //         tagItems.push({ tag: t, items: [i] });
+    //       } else {
+    //         if (index !== null) {
+    //           tagItems[index].items.push(i);
+    //         }
+    //       }
+    //     }
+    //   });
+    // }
   });
 
   return tagItems;
