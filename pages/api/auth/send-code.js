fc2dc375a76c89ed9bdfe2866d7afcf4d3b048bd
bbfrancis23 <bbfrancis23@gmail.com>
2023-08-05 09:48:52 -0700

    send code console. to track down why not sending

--- pages/api/auth/send-code.js
+++ pages/api/auth/send-code.js
@@ -1,27 +1,28 @@
-import Member from '../../../mongo/schemas/MemberSchema';
-import db from '/mongo/db';
-const nodemailer = require('nodemailer');
+import Member from '../../../mongo/schemas/MemberSchema'
+import db from '/mongo/db'
+const nodemailer = require('nodemailer')
 
-async function handler(req, res) {
+const handler = async (req, res) => {
   if (req.method === 'POST') {
-    await db.connect();
+    console.log('post trying to send reset code')
+    await db.connect()
 
     if (req.body.email) {
-      const { email } = req.body;
+      const {email} = req.body
 
       if (!email || !email.includes('@')) {
-        await db.disconnect();
-        res.status(422).json({ message: 'Invalid Input' });
-        return;
+        await db.disconnect()
+        res.status(422).json({message: 'Invalid Input'})
+        return
       }
 
-      let existingUser;
+      let existingUser = {}
       try {
-        existingUser = await Member.findOne({ email });
+        existingUser = await Member.findOne({email})
       } catch (error) {
-        await db.disconnect();
-        res.status(500).json({ message: `Server Error:  ${error}` });
-        return;
+        await db.disconnect()
+        res.status(500).json({message: `Server Error:  ${error}`})
+        return
       }
 
       if (existingUser) {
@@ -33,18 +34,16 @@ async function handler(req, res) {
             user: 'admin@brian-francis.net',
             pass: process.env.MAIL,
           },
-        });
+        })
 
-        const code = Math.floor(100000 + Math.random() * 900000);
-        const authTime = new Date();
+        console.log('found user')
+        const code = Math.floor(100000 + Math.random() * 900000)
+        const authTime = new Date()
 
         try {
-          await Member.updateOne(
-            { email },
-            { $set: { authTime, authCode: code } }
-          );
+          await Member.updateOne({email}, {$set: {authTime, authCode: code}})
         } catch (e) {
-          console.log(e);
+          console.log(e)
         }
 
         const mailOptions = {
@@ -52,29 +51,30 @@ async function handler(req, res) {
           to: email,
           subject: 'Reset Instructions',
           text: `Use this code to reset your password: ${code}`,
-        };
+        }
 
+        console.log('try to send code')
         transporter.sendMail(mailOptions, (error, info) => {
           if (error) {
-            console.log(error);
+            console.log('there was an error sending code', error)
           } else {
-            console.log('Email sent: ' + info.response);
+            console.log('Email sent: ' + info.response)
           }
-        });
+        })
 
-        await db.disconnect();
+        await db.disconnect()
         res.status(200).json({
           message: 'Reset Email sent',
-        });
+        })
       } else {
-        await db.disconnect();
-        res.status(404).json({ message: 'User not found' });
+        await db.disconnect()
+        res.status(404).json({message: 'User not found'})
       }
     } else {
-      await db.disconnect();
-      res.status(422).json({ message: 'Invalid Input' });
+      await db.disconnect()
+      res.status(422).json({message: 'Invalid Input'})
     }
   }
 }
 
-export default handler;
+export default handler
