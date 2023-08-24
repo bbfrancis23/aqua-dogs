9a8e57ae3f8d8175329fe98561d322e0cd2f3509
bbfrancis23 <bbfrancis23@gmail.com>
2023-08-23 17:50:07 -0700

    comment comming featured on item

--- pages/api/items/favorite/[itemId].js
+++ pages/api/items/favorite/[itemId].js
@@ -8,84 +8,85 @@ import {getMember} from '/mongo/controllers/memberControllers'
 import {getSession} from 'next-auth/react'
 
 export const handler = async (req, res) => {
-  const {itemId} = req.query
+  // const {itemId} = req.query
 
-  let status = 405
-  let message = 'Invalid Method'
-  let isFavorite = false
-  let member = null
+  // let status = 405
+  // let message = 'Invalid Method'
+  // let isFavorite = false
+  // let member = null
 
-  await db.connect()
+  // await db.connect()
 
-  if (req.method === 'PATCH') {
-    const session = await getSession({req})
+  // if (req.method === 'PATCH') {
+  //   const session = await getSession({req})
 
-    if (session) {
-      try {
-        member = await Member.findById(session.user.id).populate({
-          path: 'roles',
-          model: Role,
-        })
-      } catch (e) {
-        status = 500
-        message = `Server Error: ${e}`
-      }
+  //   if (session) {
+  //     try {
+  //       member = await Member.findById(session.user.id).populate({
+  //         path: 'roles',
+  //         model: Role,
+  //       })
+  //     } catch (e) {
+  //       status = 500
+  //       message = `Server Error: ${e}`
+  //     }
 
-      if (member) {
-        if (member.favoriteItems) {
-          const favFound = member.favoriteItems.filter((i) => i !== itemId)
+  //     if (member) {
+  //       if (member.favoriteItems) {
+  //         const favFound = member.favoriteItems.filter((i) => i !== itemId)
 
-          if (favFound.length > 0) {
-            member.favoriteItems = member.favoriteItems.filter((i) => i === itemId)
-            isFavorite = false
-          } else {
-            await member.favoriteItems.push(itemId)
-            isFavorite = true
-          }
-        } else {
-          await member.favoriteItems.push(itemId)
-        }
+  //         if (favFound.length > 0) {
+  //           member.favoriteItems = member.favoriteItems.filter((i) => i === itemId)
+  //           isFavorite = false
+  //         } else {
+  //           await member.favoriteItems.push(itemId)
+  //           isFavorite = true
+  //         }
+  //       } else {
+  //         await member.favoriteItems.push(itemId)
+  //       }
 
-        await member.save()
+  //       await member.save()
 
-        member = await member.toObject({getters: true, flattenMaps: true})
+  //       member = await member.toObject({getters: true, flattenMaps: true})
 
-        status = 200
-        message = 'favorite action'
-      }
-    }
-  } else if (req.method === 'GET') {
-    const session = await getSession({req})
+  //       status = 200
+  //       message = 'favorite action'
+  //     }
+  //   }
+  // } else if (req.method === 'GET') {
+  //   const session = await getSession({req})
 
-    if (session) {
-      try {
-        member = await Member.findById(session.user.id).populate({
-          path: 'roles',
-          model: Role,
-        })
-      } catch (e) {
-        status = 500
-        message = `Server Error: ${e}`
-      }
+  //   if (session) {
+  //     try {
+  //       member = await Member.findById(session.user.id).populate({
+  //         path: 'roles',
+  //         model: Role,
+  //       })
+  //     } catch (e) {
+  //       status = 500
+  //       message = `Server Error: ${e}`
+  //     }
 
-      if (member) {
-        if (member.favoriteItems) {
-          const favoriteFound = member.favoriteItems.filter((i) => i.toString() === itemId)
+  //     if (member) {
+  //       if (member.favoriteItems) {
+  //         const favoriteFound = member.favoriteItems.filter((i) => i.toString() === itemId)
 
-          if (favoriteFound.length > 0) {
-            isFavorite = true
-          }
-        }
-        status = 200
-        message = ''
-      }
-    }
-  }
-  await db.disconnect()
-  res.status(status).json({
-    message,
-    isFavorite,
-  })
+  //         if (favoriteFound.length > 0) {
+  //           isFavorite = true
+  //         }
+  //       }
+  //       status = 200
+  //       message = ''
+  //     }
+  //   }
+  // }
+  // await db.disconnect()
+  // res.status(status).json({
+  //   message,
+  //   isFavorite,
+  // })
+  res.status(200).json({message: 'comming soon'})
 }
 
 export default handler
--- pages/api/items/vote/[itemId].js
+++ pages/api/items/vote/[itemId].js
@@ -20,61 +20,62 @@ const processVote = async (item, vote, userId) => {
 }
 
 export default async function handler(req, res) {
-  const {itemId} = req.query
-  let status = 405
-  let message = 'Invalid Method'
-  let item = {}
-  await db.connect()
+  // const {itemId} = req.query
+  // let status = 405
+  // let message = 'Invalid Method'
+  // let item = {}
+  // await db.connect()
 
-  if (req.method === 'PATCH') {
-    const session = await getSession({req})
+  // if (req.method === 'PATCH') {
+  //   const session = await getSession({req})
 
-    if (session) {
-      try {
-        item = await Item.findById(itemId).populate({
-          path: 'sections',
-          model: Section,
-        })
-      } catch (e) {
-        status = 500
-        message = `Server Error: ${e}`
-      }
+  //   if (session) {
+  //     try {
+  //       item = await Item.findById(itemId).populate({
+  //         path: 'sections',
+  //         model: Section,
+  //       })
+  //     } catch (e) {
+  //       status = 500
+  //       message = `Server Error: ${e}`
+  //     }
 
-      if (item) {
-        const {vote} = req.body
+  //     if (item) {
+  //       const {vote} = req.body
 
-        if (vote) {
-          if (vote === 'up' || vote === 'down' || vote === 'reset') {
-            item = await processVote(item, vote, session.user.id)
-            await item.save()
+  //       if (vote) {
+  //         if (vote === 'up' || vote === 'down' || vote === 'reset') {
+  //           item = await processVote(item, vote, session.user.id)
+  //           await item.save()
 
-            item = await item.toObject({getters: true, flattenMaps: true})
+  //           item = await item.toObject({getters: true, flattenMaps: true})
 
-            item = await flattenItem(item)
+  //           item = await flattenItem(item)
 
-            status = 200
-            message = 'Vote Registered'
-          } else {
-            status = 400
-            message = 'Invalid Vote, please vote "up" or "down"'
-          }
-        } else {
-          status = 400
-          message = 'No vote field'
-        }
-      } else {
-        status = 404
-        message = 'Invalid Item'
-      }
-    } else {
-      status = 403
-      message = 'Permission Denied'
-    }
-  }
+  //           status = 200
+  //           message = 'Vote Registered'
+  //         } else {
+  //           status = 400
+  //           message = 'Invalid Vote, please vote "up" or "down"'
+  //         }
+  //       } else {
+  //         status = 400
+  //         message = 'No vote field'
+  //       }
+  //     } else {
+  //       status = 404
+  //       message = 'Invalid Item'
+  //     }
+  //   } else {
+  //     status = 403
+  //     message = 'Permission Denied'
+  //   }
+  // }
 
-  await db.disconnect()
-  res.status(status).json({
-    message,
-    item,
-  })
+  // await db.disconnect()
+  // res.status(status).json({
+  //   message,
+  //   item,
+  // })
+  res.status(200).json({message: 'comming soon'})
 }
